ff0361c6e4774458e2fda3d2be197e6572830686
Diego <djpg98@gmail.com>
2021-07-02 20:35:09 -0400

    Commented the code beacuse it was difficult to understand without comments

--- othello_cut.h
+++ othello_cut.h
@@ -26,6 +26,7 @@
 #define DIM           36
 #define N             6
 
+/* Para cada posición en pos, las posiciones que están en la misma fila*/
 const int rows[][7] = {
     { 4, 5, 6, 7, 8, 9,-1 }, { 4, 5, 6, 7, 8, 9,-1 }, { 4, 5, 6, 7, 8, 9,-1 },
     { 4, 5, 6, 7, 8, 9,-1 }, { 4, 5, 6, 7, 8, 9,-1 }, { 4, 5, 6, 7, 8, 9,-1 },
@@ -40,6 +41,8 @@ const int rows[][7] = {
     {30,31,32,33,34,35,-1 }, {30,31,32,33,34,35,-1 }, {30,31,32,33,34,35,-1 },
     {30,31,32,33,34,35,-1 }, {30,31,32,33,34,35,-1 }, {30,31,32,33,34,35,-1 }
 };
+
+/* Para cada posición en pos, las posiciones que están en la misma columna*/
 const int cols[][7] = {
     { 4,10,16,20,24,30,-1 }, { 5,11,17,21,25,31,-1 }, { 6,12, 0, 2,26,32,-1 },
     { 7,13, 1, 3,27,33,-1 }, { 8,14,18,22,28,34,-1 }, { 9,15,19,23,29,35,-1 },
@@ -54,6 +57,8 @@ const int cols[][7] = {
     { 4,10,16,20,24,30,-1 }, { 5,11,17,21,25,31,-1 }, { 6,12, 0, 2,26,32,-1 },
     { 7,13, 1, 3,27,33,-1 }, { 8,14,18,22,28,34,-1 }, { 9,15,19,23,29,35,-1 }
 };
+
+/* Para cada posición en pos, las posiciones que están en la misma diagonal 1*/
 const int dia1[][7] = {
     { 4,11, 0, 3,28,35,-1 }, { 5,12, 1,22,29,-1,-1 }, { 6,13,18,23,-1,-1,-1 },
     { 7,14,19,-1,-1,-1,-1 }, { 8,15,-1,-1,-1,-1,-1 }, { 9,-1,-1,-1,-1,-1,-1 },
@@ -68,6 +73,8 @@ const int dia1[][7] = {
     {30,-1,-1,-1,-1,-1,-1 }, {24,31,-1,-1,-1,-1,-1 }, {20,25,32,-1,-1,-1,-1 },
     {16,21,26,33,-1,-1,-1 }, {10,17, 2,27,34,-1,-1 }, { 4,11, 0, 3,28,35,-1 }
 };
+
+/* Para cada posición en pos, las posiciones que están en la misma diagonal 2*/
 const int dia2[][7] = {
     { 4,-1,-1,-1,-1,-1,-1 }, { 5,10,-1,-1,-1,-1,-1 }, { 6,11,16,-1,-1,-1,-1 },
     { 7,12,17,20,-1,-1,-1 }, { 8,13, 0,21,24,-1,-1 }, { 9,14, 1, 2,25,30,-1 },
@@ -91,39 +98,62 @@ static int PV[] = {
 };
 
 class state_t {
-    unsigned char t_; 
-    unsigned free_;
-    unsigned pos_;
+    /* Básicamente, esta representación toma las 4 casillas del centro, que siempre van a estar llenas, y dice que esas 
+       son las posiciones de la 0 a la 3. Y esas las representa en t_.
+    */
+
+    unsigned char t_; //OK, esto guarda la información de las 4 casillas del centro. Particularmente, los 4 bits más a la derecha son los que complementan pos_, 
+    unsigned free_; //Indica si las 32 casillas restantes están libres o no
+    unsigned pos_; //Indica si las 32 casillas restantes tienen piezas negras
 
   public:
-    explicit state_t(unsigned char t = 6) : t_(t), free_(0), pos_(0) { }
+    explicit state_t(unsigned char t = 6) : t_(t), free_(0), pos_(0) { } //Constructor
 
+    //Getters
     unsigned char t() const { return t_; }
     unsigned free() const { return free_; }
     unsigned pos() const { return pos_; }
+    //Hashing current state
     size_t hash() const { return free_ ^ pos_ ^ t_; }
 
+    /*
+    @color: True si el color es negro, False si blanco
+    @pos: La posición en el tablero para la cual queremos ver el color de la ficha
+    @returns: True si la pieza en la casilla es del color solicitado, False si no o no hay pieza ahí
+    */
     bool is_color(bool color, int pos) const {
         if( color )
             return pos < 4 ? t_ & (1 << pos) : pos_ & (1 << (pos - 4));
         else
             return !(pos < 4 ? t_ & (1 << pos) : pos_ & (1 << (pos - 4)));
     }
+
+    /* Estas 4 el título es bastante obvio*/
     bool is_black(int pos) const { return is_color(true, pos); }
     bool is_white(int pos) const { return is_color(false, pos); }
     bool is_free(int pos) const { return pos < 4 ? false : !(free_ & (1 << (pos - 4))); }
     bool is_full() const { return ~free_ == 0; }
 
+    /*Cuenta el número de "Puntos" de las piezas negras: 1 por cada pieza negra y -1 por cada pieza blanca*/
     int value() const;
+    /*Determina si llegamos a un estado terminal: Se llenó el tablero o nadie puede jugar*/
     bool terminal() const;
+    /* Si es posible poner una pieza de color "color" en la posición "pos" (90% seguro)*/
     bool outflank(bool color, int pos) const;
+    /* Si eiste una posición válida para poner una ficha negra*/
     bool is_black_move(int pos) const { return (pos == DIM) || outflank(true, pos); }
+    /* Si eiste una posición válida para poner una ficha blanca*/
     bool is_white_move(int pos) const { return (pos == DIM) || outflank(false, pos); }
 
+    /* Cambia el color de la posición pos*/
     void set_color(bool color, int pos);
+    /* Poner una ficha de color "color" en la posición "pos"*/
     state_t move(bool color, int pos) const;
+    /* Poner una ficha de color negro en la posición "pos" */
     state_t black_move(int pos) { return move(true, pos); }
+    /* Poner una ficha de color blanca en la posición "pos" */
     state_t white_move(int pos) { return move(false, pos); }
+    /*Movimiento random para el jugador "color"*/
     int get_random_move(bool color) {
         std::vector<int> valid_moves;
         for( int pos = 0; pos < DIM; ++pos ) {
@@ -134,6 +164,7 @@ class state_t {
         return valid_moves.empty() ? -1 : valid_moves[lrand48() % valid_moves.size()];
     }
 
+    //Operaciones de comparación y asignación
     bool operator<(const state_t &s) const {
         return (free_ < s.free_) || ((free_ == s.free_) && (pos_ < s.pos_));
     }
@@ -147,6 +178,7 @@ class state_t {
         return *this;
     }
 
+    //Output, posiblemente formateado.
     void print(std::ostream &os, int depth = 0) const;
     void print_bits(std::ostream &os) const;
 };
@@ -302,6 +334,7 @@ inline void state_t::print_bits(std::ostream &os) const {
     for( int i = 31; i >= 0; --i ) os << (free_ & (1 << i) ? '1' : '0');
 }
 
+/*Esto hace algo con el print, pero no estoy claro, no me senté a leer la implementación de print*/
 inline std::ostream& operator<<(std::ostream &os, const state_t &state) {
     state.print(os);
     return os;
